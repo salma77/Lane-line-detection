cbea1ec674ed4db8f817edd07595612763a62a13
salma77 <salmaabdelfatah77@gmail.com>
2022-04-20 08:59:28 +0200

    added binary thresholding function

---
+++ binary_thresholding.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def binary_thresholded(img, debug=False):
+    """
+    Function that applies a binary threshold to an image
+    Parameters:
+        img (np.array): Image to be thresholded
+        Returns:
+            img_binary (np.array): Thresholded image
+    """
+    img = cv2.imread(img)
+    # Transform image to gray scale
+    gray_img =cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Apply sobel (derivative) in x direction, this is usefull to detect lines that tend to be vertical
+    sobelx = cv2.Sobel(gray_img, cv2.CV_64F, 1, 0)
+    abs_sobelx = np.absolute(sobelx)
+    # Scale result to 0-255
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+    sx_binary = np.zeros_like(scaled_sobel)
+    # Keep only derivative values that are in the margin of interest
+    sx_binary[(scaled_sobel >= 30) & (scaled_sobel <= 255)] = 1
+
+    # Detect pixels that are white in the grayscale image
+    white_binary = np.zeros_like(gray_img)
+    white_binary[(gray_img > 200) & (gray_img <= 255)] = 1
+
+    # Convert image to HLS
+    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    H = hls[:,:,0]
+    S = hls[:,:,2]
+    sat_binary = np.zeros_like(S)
+    # Detect pixels that have a high saturation value
+    sat_binary[(S > 90) & (S <= 255)] = 1
+
+    hue_binary =  np.zeros_like(H)
+    # Detect pixels that are yellow using the hue component
+    hue_binary[(H > 10) & (H <= 25)] = 1
+
+    # Combine all pixels detected above
+    binary_1 = cv2.bitwise_or(sx_binary, white_binary)
+    binary_2 = cv2.bitwise_or(hue_binary, sat_binary)
+    binary = cv2.bitwise_or(binary_1, binary_2)
+    if debug:
+        plt.imshow(binary, cmap='gray')
+        # plt.savefig('/home/salma/My_Code/cse/img-process/Lane-line-detection/binary_thresholded.png', format='png', dpi=300)
+        cv2.imwrite('/home/salma/My_Code/cse/img-process/Lane-line-detection/binary_thresholded.jpeg', binary*255)
+    return binary
